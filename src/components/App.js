2f0c635c2af9db99941fe3260700034529983b71
ghiyaee <86221396+ghiyaee@users.noreply.github.com>
2022-05-05 01:30:14 +0430

    CHNAGE COLORS

--- src/components/App.js
+++ src/components/App.js
@@ -43,7 +43,7 @@ const App = () => {
         setCalenders(filter)
     }
     const onMove = (e) => {
-        setInput(e)
+         setInput(e)
          setDrop(true)
          setText('EDIT')
     }
